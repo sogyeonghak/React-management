b4df6bf86dd48f8a77138c4bcd2d35ec1779dafd
sogyeonghak <sogyeonghak@ajou.ac.kr>
2021-02-22 16:38:14 +0900

    Create Customer.js

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import logo from './logo.svg';
+import './App.css';
+import Customer from './components/Customer'
+
+const customer =[
+{
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '홍길동',
+  'birthday' : "961222",
+  'gender' : '남자',
+  'job' : '대학생'
+},
+
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '고길동',
+  'birthday' : "960322",
+  'gender' : '남자',
+  'job' : '대학생'  
+},
+
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '소길동',
+  'birthday' : "951002",
+  'gender' : '남자',
+  'job' : '대학생'
+}
+]
+function App(){
+    return (
+      <div>
+        {
+          customer.map(c=>{
+            return(
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+          })
+        }
+      </div>
+
+    );
+}
+
+export default App;
